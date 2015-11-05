fa9ea211f9f1154080390f41bb54520083920643
Stefan <stefan.wuertenberger@ametras.com>
2015-11-05 11:31:05 +0100

    Add allen_sauer Log

--- GwtTable/src/com/swtools/gwttable/client/GwtTable.java
+++ GwtTable/src/com/swtools/gwttable/client/GwtTable.java
@@ -1,5 +1,7 @@
 package com.swtools.gwttable.client;
 
+
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -13,6 +15,8 @@ public class GwtTable implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		Log.info("- onModuleLoad -");
 		RootPanel.get().add(new Label("CLEAN"));
+		
 	}
 }
